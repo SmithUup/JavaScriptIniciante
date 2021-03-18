981f6d8c43427b59b6222894c8d35dd3c5b54097
SmithUup <smith@tismith.com.br>
2021-03-18 19:05:49 -0300

    Funções e Operações matemáticas.

--- script.js
+++ script.js
@@ -5,3 +5,58 @@ Isso é um
 comentário de
 multiplas linhas.
 */
+
+//Criando a primeira função.
+function acao()
+{
+    var area = document.getElementById("area");
+    var texto = prompt("Qual seu nome?");
+
+    area.innerHTML=texto;
+}
+
+function sobrenome(nome)
+{
+    var snome = document.getElementById("SobreNome");
+    var tsnome = prompt("Qual seu sobrenome?");
+
+    SobreNome.innerHTML=nome+" "+tsnome;
+}
+
+function addingrediente()
+{
+    //caputurando o valor de uma input
+    var ing = document.getElementById("igrediante").value;
+
+    //caputrando o conteúdo html desse id
+    var listahtml = document.getElementById("lista").innerHTML;
+
+    listahtml = listahtml + "<li>"+ing+"</li>";
+
+    document.getElementById("lista").innerHTML=listahtml;
+
+}
+
+function somar()
+{
+//Recebendo um interior parseInt    
+var n1 = parseInt(document.getElementById("n1").value);
+var n2 = parseInt(document.getElementById("n2").value);
+alert(n1+n2);
+}
+
+function multi()
+{
+//Recebendo um interior parseInt    
+var n1 = parseInt(document.getElementById("n1").value);
+var n2 = parseInt(document.getElementById("n2").value);
+alert(n1*n2);
+}
+
+function div()
+{
+//Recebendo um interior parseInt    
+var n1 = parseInt(document.getElementById("n1").value);
+var n2 = parseInt(document.getElementById("n2").value);
+alert(n1/n2);
+}
