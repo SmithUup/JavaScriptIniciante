5efc411e7f4005315db3f8aa7bf448090e0a53c8
SmithUup <smith@tismith.com.br>
2021-03-20 11:16:08 -0300

    Adicionando README, Inicializando array com um pequeno verificado IF.

--- script.js
+++ script.js
@@ -60,3 +60,69 @@ var n1 = parseInt(document.getElementById("n1").value);
 var n2 = parseInt(document.getElementById("n2").value);
 alert(n1/n2);
 }
+
+//array, em global
+var lista = ["Feijão", "Arroz", "Batata", "Pera", "Uva", "Tamara", "Ovos", "Banana"];
+
+//Transformando o array em string separando por virgula e espaço.
+var ListaString = lista.join(", ");
+
+function VerLista()
+{ 
+    var ListaInfor = document.getElementById("ListaInfor");
+    ListaInfor.innerHTML = ListaString;
+}
+
+function posicao()
+{
+    var posicaotxt = document.getElementById("posicaotxt").value; 
+    var ListaInfor = document.getElementById("ListaInfor");
+
+    //Capturando a posição do array
+    var posicaonumber = lista.indexOf(posicaotxt);
+    
+    //Se o retorno da busca for menor que 0, o valor não existe.
+    if (posicaonumber < 0)
+    {
+        ListaInfor.innerHTML = "Valor não encontrado.";
+    }
+    else
+    {
+        ListaInfor.innerHTML = posicaonumber;
+    }
+}
+
+function quantidade()
+{
+ var ListaInfor = document.getElementById("ListaInfor");
+
+ //Exibindo o total de elementos de um array
+ ListaInfor.innerHTML = lista.length;
+}
+
+function removeListaUltimo()
+{
+    //remove o último item de um array
+    lista.pop();
+    var ListaInfor = document.getElementById("ListaInfor");
+    ListaInfor.innerHTML = lista;
+
+}
+
+function removeListaPrimeiro()
+{
+    //remove o primeiro item de um array
+    lista.shift();
+    var ListaInfor = document.getElementById("ListaInfor");
+    ListaInfor.innerHTML = lista;
+
+}
+
+function Addarray()
+{
+    var ArrayTxt = document.getElementById("ArrayTxt").value;
+    //Adiciona um item em um array.
+    lista.push(ArrayTxt);
+    ListaInfor.innerHTML = lista;
+}
+
