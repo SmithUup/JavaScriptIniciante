73d7323a6baea6e75ecb93ac636564a58b2bd63a
SmithUup <smith@tismith.com.br>
2021-03-31 14:43:35 -0300

    Iniciando a função auto e inserindo score.

--- play.js
+++ play.js
@@ -12,11 +12,27 @@ bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
 
 bola.setAttribute("onclick", "estourar(this)");
 
+//appendChild add o elelmento na tela
 document.body.appendChild(bola);
 
 }
 
+var ponto = 0;
 function estourar(bolha)
 {
+ //removeChild remove o elemento da tela   
  document.body.removeChild(bolha);
+
+ ponto = ponto + 10;
+ document.getElementById("pontos").innerHTML = ponto;
 }
+
+function play()
+{
+    //roda uma função a cada 1 segundo
+    setInterval(addbola, 1000);
+    
+}
+
+
+ 
