162cc6fece11e9bec345fd17cb2ff1c26fadc541
SmithUup <smith@tismith.com.br>
2021-04-02 14:29:27 -0300

    Adicionando red ball com opção 50ps, finalizando a versão 1.0

--- play.js
+++ play.js
@@ -1,37 +1,68 @@
-function addbola()
+function green()
 {
 
-var bola = document.createElement("div");
-bola.setAttribute("class", "bola");
+var bolaverde10 = document.createElement("div");
+bolaverde10.setAttribute("class", "bolaverde10");
 
 //math.randon cria um números decimais aleatório, o Math.floor faz desse número um interior, eliminando a flutuação.
-var p1 = Math.floor(Math.random() * 500);
-var p2 = Math.floor(Math.random() * 400);
+var pg1 = Math.floor(Math.random() * 1300);
 
-bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+// left=400px top=60px
+var pg2 = Math.floor(Math.random() * (400) + 60);
 
-bola.setAttribute("onclick", "estourar(this)");
+bolaverde10.setAttribute("style", "left:"+pg1+"px;top:"+pg2+"px;");
+
+bolaverde10.setAttribute("onclick", "estourarverde(this)");
 
 //appendChild add o elelmento na tela
-document.body.appendChild(bola);
+document.body.appendChild(bolaverde10);
 
 }
 
+
+function red()
+{
+
+var bolavemelha50 = document.createElement("div");
+bolavemelha50.setAttribute("class", "bolavemelha50");
+
+var pr1 = Math.floor(Math.random() * 1000);
+var pr2 = Math.floor(Math.random() * (400) + 60);
+
+bolavemelha50.setAttribute("style", "left:"+pr1+"px;top:"+pr2+"px;");
+bolavemelha50.setAttribute("onclick", "estourarvermelho(this)");
+
+document.body.appendChild(bolavemelha50);
+
+}
+
+
 var ponto = 0;
-function estourar(bolha)
+function estourarverde(verde)
 {
  //removeChild remove o elemento da tela   
- document.body.removeChild(bolha);
+ document.body.removeChild(verde);
 
  ponto = ponto + 10;
  document.getElementById("pontos").innerHTML = ponto;
 }
 
+
+function estourarvermelho(vermelha)
+{
+ //removeChild remove o elemento da tela   
+ document.body.removeChild(vermelha);
+
+ ponto = ponto + 50;
+ document.getElementById("pontos").innerHTML = ponto;
+}
+
+
 function play()
 {
-    //roda uma função a cada 1 segundo
-    setInterval(addbola, 1000);
-    
+ //roda uma função a cada 1 segundo
+ setInterval(green, 1000);
+ setInterval(red, 10000); 
 }
 
 
